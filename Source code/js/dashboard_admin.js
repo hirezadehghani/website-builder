dfe47f70b2a617653aa21ba82d97385426a87790
AmirHossein-z <amirhossein.zareian.engineer@gmail.com>
2022-02-21 19:34:32 +0330

    dashboard_admin.js edited for for readable

--- Source code/js/dashboard_admin.js
+++ Source code/js/dashboard_admin.js
@@ -15,7 +15,7 @@ let returnBtnMobile = document.querySelector("#returnButtonMobile");
    close the panel through it. so this Event listener plays role
    when we click on hamburger menu.
 */
-hamburgerMenu.addEventListener("click", () => {
+let showPanelMobile = () => {
     if (panel.classList.contains("hidden")) {
         hamburgerMenu.classList.add("hidden");
         exitBtnMobile.classList.add("hidden");
@@ -23,76 +23,83 @@ hamburgerMenu.addEventListener("click", () => {
         panel.classList.remove("hidden", "opacity-0");
         closeArrowBtn.classList.remove("hidden");
     }
-});
+};
+
 /* when we want to close panel this event listener should execute 
    and close panel and show hamburger menu
 */
-closeArrowBtn.addEventListener("click", () => {
+let closePanelMobile = () => {
     panel.classList.add("hidden", "opacity-0");
     closeArrowBtn.classList.add("hidden");
     hamburgerMenu.classList.remove("hidden");
     exitBtnMobile.classList.remove("hidden");
     returnBtnMobile.classList.remove("hidden");
-})
+}
 
 /* when click on one item of panel, that item should get
    background-color secondary and related information to
    that section will display and other section should be
    disappeared
 */
-panelListTag[2].classList.add("bg-secondary");
-sections[0].classList.remove("hidden");
-/* we start this loop from item 3 (i=2) because first two
-   item have no special function and be displayed just for
-   info to user
-*/
-for (let i = 2; i < panelListTag.length; i++) {
+let showSpecialSection = () => {
 
-    panelListTag[i].addEventListener("click", () => {
-        // remove background color secondary class for other item
-        document.querySelectorAll(".right-panel ul li.bg-secondary").forEach((el) => {
-            el.classList.remove("bg-secondary");
-        });
-        // add hidden class to divs tag that don't have that class
-        // because just one item should be displayed to user
-        document.querySelectorAll(".sections > div").forEach((sec) => {
-            if (!sec.classList.contains("hidden")) {
-                sec.classList.add("hidden");
-            }
-        });
+    panelListTag[2].classList.add("bg-secondary");
+    sections[0].classList.remove("hidden");
+    /* we start this loop from item 3 (i=2) because first two
+       item have no special function and be displayed just for
+       info to user
+    */
+    for (let i = 2; i < panelListTag.length; i++) {
 
-        panelListTag[i].classList.add("bg-secondary");
-        sections[i - 2].classList.remove("hidden");
-        // if user first click on ' تغییر جزئیات ' and then click on
-        // other li tag this section should be disappeared
-        changeInfoSection.classList.add("hidden");
-    });
+        panelListTag[i].addEventListener("click", () => {
+            // remove background color secondary class for other item
+            document.querySelectorAll(".right-panel ul li.bg-secondary").forEach((el) => {
+                el.classList.remove("bg-secondary");
+            });
+            /* add hidden class to divs tag that don't have that class
+               because just one item should be displayed to user
+            */
+            document.querySelectorAll(".sections > div").forEach((sec) => {
+                if (!sec.classList.contains("hidden")) {
+                    sec.classList.add("hidden");
+                }
+            });
+
+            panelListTag[i].classList.add("bg-secondary");
+            sections[i - 2].classList.remove("hidden");
+            /* if user first click on ' تغییر جزئیات ' and then click on
+               other li tag this section should be disappeared
+            */
+            changeInfoSection.classList.add("hidden");
+        });
+    }
 }
 
-// when user click on 'تغییر جزئیات' that section should be 
-// disappeared and change info section be displayed to user
-changeInfoBtn.addEventListener("click", () => {
+/* when user click on 'تغییر جزئیات' that section should be 
+   disappeared and change info section be displayed to user
+*/
+let showDetailsInfoSection = () => {
     sections[0].classList.add("hidden");
     changeInfoSection.classList.remove("hidden");
-});
+    returnBtnMobile.classList.remove("hidden");
+};
 
 /* when user click the arrow left button that section
    should be disappeared and section admin info be
-   displayed to user */
-changeInfoSection.children[0].addEventListener("click", () => {
+   displayed to user
+*/
+let returnToUserInfoSection = () => {
     sections[0].classList.remove("hidden");
     changeInfoSection.classList.add("hidden");
-});
-returnBtnMobile.addEventListener("click", () => {
-    sections[0].classList.remove("hidden");
-    changeInfoSection.classList.add("hidden");
-});
+};
 
 /* when user click on 'خروج' button an alert should
    should be displayed to warn user that you are
-   loging out from panel */
+   loging out from panel
+*/
 let exitFromPanel = () => {
 
+    // using sweetalert2 for create a beautiful alert
     Swal.fire({
         title: 'خروج',
         text: 'آیا برای خارج شدن از پنل کاربری مطمئن هستید؟ ',
@@ -109,14 +116,15 @@ let exitFromPanel = () => {
         }
     });
 };
-exitBtn.addEventListener("click", exitFromPanel);
-exitBtnMobile.addEventListener("click", exitFromPanel);
+
 
 /* when user click on 'حذف حساب کاربری' button an alert
    should be displayed to warn user that you are deleting
-   your account permanently */
+   your account permanently 
+*/
 let deleteAccount = () => {
 
+    // using sweetalert2 for create a beautiful alert
     Swal.fire({
         title: 'اخطار',
         text: 'آیا از حذف حساب کاربری خود مطمئن هستید؟ ',
@@ -139,4 +147,13 @@ let deleteAccount = () => {
         }
     });
 };
+
+hamburgerMenu.addEventListener("click", showPanelMobile);
+closeArrowBtn.addEventListener("click", closePanelMobile);
+showSpecialSection();
+changeInfoBtn.addEventListener("click", showDetailsInfoSection);
+changeInfoSection.children[0].addEventListener("click", returnToUserInfoSection);
+returnBtnMobile.addEventListener("click", returnToUserInfoSection);
+exitBtn.addEventListener("click", exitFromPanel);
+exitBtnMobile.addEventListener("click", exitFromPanel);
 deleteAccountBtn.addEventListener("click", deleteAccount);
