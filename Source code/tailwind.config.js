0d99f980379ecfef88533e17cbed42f4746756f5
AmirHossein-z <amirhossein.zareian.engineer@gmail.com>
2022-03-08 22:04:38 +0330

    in tailwind config, all javascript file should be added for supporting tainwind class

--- Source code/tailwind.config.js
+++ Source code/tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  content: ["./*.html", "./pages/*/*.html"],
-  theme: {
-    extend: {
-      colors: {
-        'primary': '#000051',
-        'secondary': '#7200CA',
-      },
+    content: ["./*.html", "./pages/*/*.html", "./js/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#000051",
+                secondary: "#7200CA",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
