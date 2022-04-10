45aa3b9b6cf09fab89cda2f99a3fa481bd37afe5
AmirHossein-z <amirhossein.zareian.engineer@gmail.com>
2022-04-10 18:51:08 +0430

    validation for list users section.mjs file was added

--- Source code/js/dashboardAdminFiles/listUsersSection.mjs
+++ Source code/js/dashboardAdminFiles/listUsersSection.mjs
@@ -3,6 +3,60 @@ let usersListTableContent = document.querySelectorAll(
     "#usersListTableContent > div"
 );
 
+let persianNumber = {
+    "۰": 0,
+    "۱": 1,
+    "۲": 2,
+    "۳": 3,
+    "۴": 4,
+    "۵": 5,
+    "۶": 6,
+    "۷": 7,
+    "۸": 8,
+    "۹": 9,
+};
+
+let isAllpersianNumbers = (NC) => {
+    if (NC === "") {
+        return false;
+    }
+    for (let i = 0; i < NC.length; i++) {
+        if (persianNumber[NC[i]] === undefined) {
+            return false;
+        }
+    }
+    return true;
+};
+
+let phoneNumberValidation = (value) => {
+    let phoneNumber = value;
+    let pn;
+    if (isAllpersianNumbers(phoneNumber)) {
+        // convert persian number to English number
+        for (let i = 0; i < phoneNumber.length; i++) {
+            pn += persianNumber[phoneNumber[i]];
+        }
+        // checks if phoneNumber's values are all English numbers
+    } else if (parseInt(phoneNumber)) {
+        pn = phoneNumber;
+    } else {
+        return false;
+    }
+
+    if (pn.length === 10) {
+        // this regex is for all Iran's phone numbers such as Irancell,Hamrahe Aval,Rightel
+        let regexForPhoneNumber =
+            /^9(0[1-9]|1[0-9]|2[0-2]|3[0-9]|9[0-4]|41)[0-9]{7}$/;
+        if (regexForPhoneNumber.test(parseInt(pn))) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+};
+
 let deleteUsersListTableItem = (e) => {
     let tableItem = e.target.parentElement;
     Swal.fire({
@@ -61,12 +115,12 @@ let searchUsersListTable = (e) => {
 };
 
 let editUsersListTableItem = (e) => {
-    let defaultValue = e.target.innerHTML;
+    let tableItemContent = e.target;
     Swal.fire({
         title: " ویرایش محتوای جدول کاربران",
         input: "text",
         inputLabel: " مقدار جدید را وارد کنید ",
-        inputPlaceholder: defaultValue,
+        inputPlaceholder: tableItemContent.innerHTML.trim(), // trim function removes whitespace from end and begining
         confirmButtonColor: "#66bb6a",
         confirmButtonText: " تایید ",
         showCancelButton: true,
@@ -74,12 +128,55 @@ let editUsersListTableItem = (e) => {
         cancelButtonColor: "#d33",
     }).then((result) => {
         if (result.isConfirmed) {
-            e.target.innerHTML = result.value;
+            if (
+                tableItemContent.getAttribute("data-firstName") ||
+                tableItemContent.getAttribute("data-lastName")
+            ) {
+                // if our target is name of user
+                let regexForName = /^[آ-ی]{4,}\s*[آ-ی]*/;
+                if (regexForName.test(result.value)) {
+                    e.target.innerHTML = result.value;
+                } else {
+                    Swal.fire({
+                        title: " اخطار ",
+                        text: " مقدار وارد شده معتبر نمی باشد.\nباید مقدار وارد شده شامل فقط حروف فارسی و حداقل ۴ حرف باشد.",
+                        icon: "warning",
+                        confirmButtonText: "باشه",
+                        confirmButtonColor: "#65bb6a",
+                    });
+                }
+            } else if (tableItemContent.getAttribute("data-phone")) {
+                // if our target is phone number of user
+                if (phoneNumberValidation(result.value)) {
+                    e.target.innerHTML = result.value;
+                } else {
+                    Swal.fire({
+                        title: " اخطار ",
+                        text: " شماره تماس معتبری را بدون احتساب صفر وارد کنید ",
+                        icon: "warning",
+                        confirmButtonText: "باشه",
+                        confirmButtonColor: "#65bb6a",
+                    });
+                }
+            } else if (tableItemContent.getAttribute("data-mail")) {
+                // if our target is email of user
+                let regexFormail =
+                    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (regexFormail.test(result.value)) {
+                    e.target.innerHTML = result.value;
+                } else {
+                    Swal.fire({
+                        title: " اخطار ",
+                        text: " ایمیل معتبر نمی باشد ",
+                        icon: "warning",
+                        confirmButtonText: "باشه",
+                        confirmButtonColor: "#65bb6a",
+                    });
+                }
+            }
         }
     });
 };
-
-// users list section
 
 // 'حذف' button in users list section
 listUsersButtons.children[0].addEventListener("click", () => {
