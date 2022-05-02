d752a357714de0d986c1fd88b8d2c503b5fab8e7
AmirHossein-z <amirhossein.zareian.engineer@gmail.com>
2022-05-02 17:34:14 +0430

    config of tailwind was edited

--- Source code/tailwind.config.js
+++ Source code/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ["./*.html", "./pages/*/*.html", "./js/**/*.js"],
+    content: ["./*.html", "./pages/*/*.html", "./js/**/*.mjs"],
     theme: {
         extend: {
             colors: {
