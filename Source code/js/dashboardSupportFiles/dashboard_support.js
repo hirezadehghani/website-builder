05cfa8d76a16b771117d2062ad503eef13e85291
AmirHossein-z <amirhossein.zareian.engineer@gmail.com>
2022-05-03 11:46:38 +0430

    main page of dashboard support was created

---
+++ Source code/js/dashboardSupportFiles/dashboard_support.js
@@ -0,0 +1,6 @@
+// from admin files
+import "../dashboardAdminFiles/panelMobile.mjs";
+import "../dashboardAdminFiles/exitFromPanel.mjs";
+import "../dashboardAdminFiles/deleteAccount.mjs";
+// **********************************************************
+import "../dashboardSupportFiles/handleDisplayOfSections.mjs";
