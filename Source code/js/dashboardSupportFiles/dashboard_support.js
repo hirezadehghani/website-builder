3f7db40fd6ab2fdf2907c20a5c87075eeeb8ce18
AmirHossein-z <amirhossein.zareian.engineer@gmail.com>
2022-05-03 11:56:29 +0430

    change user info section was added

--- Source code/js/dashboardSupportFiles/dashboard_support.js
+++ Source code/js/dashboardSupportFiles/dashboard_support.js
@@ -2,5 +2,6 @@
 import "../dashboardAdminFiles/panelMobile.mjs";
 import "../dashboardAdminFiles/exitFromPanel.mjs";
 import "../dashboardAdminFiles/deleteAccount.mjs";
+import "../dashboardAdminFiles/formValidationFromUserInfo.mjs";
 // **********************************************************
 import "../dashboardSupportFiles/handleDisplayOfSections.mjs";
